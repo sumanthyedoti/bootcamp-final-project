9d211da2577b5840b58593f41d89a0862e7a3a15
Ram Sahu <ram.sahu@mountblue.io>
2019-02-15 20:21:46 +0530

    after merge

--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,15 @@ import {Provider} from 'react-redux';
 import store from './store/store'
 import './index.css';
 import App from './App';
- import AddMemberOrg from './components/OragnisationPanel/AddMemberOrg'
- import DeleteMemberOrg from './components/OragnisationPanel/DeleteMemberOrg';
+//  import AddMemberOrg from './components/OragnisationPanel/AddMemberOrg'
+//  import DeleteMemberOrg from './components/OragnisationPanel/DeleteMemberOrg';
 // import AddNewGroup from './components/OragnisationPanel/AddNewGroup';
 // import AddMemberGroup from './components/OragnisationPanel/AddMemberGroup';
 // import DeleteMemberGroup from './components/OragnisationPanel/DeleteMemberGroup'
 
 // import Test from './components/OragnisationPanel/OrganizationTask';
-import TaskInCompplet from './components/GroupPanel/TaskinComplete'
-import TaskCompplet from './components/GroupPanel/TaskComplete'
+// import TaskInCompplet from './components/GroupPanel/TaskinComplete'
+// import TaskCompplet from './components/GroupPanel/TaskComplete'
 ReactDOM.render(
   <Provider store={store}>
     <App />
